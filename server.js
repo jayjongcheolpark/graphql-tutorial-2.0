03ab1eed31b6071df7ad7750b38b83400600efb7
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-10 15:27:30 -0500

    Add schema and mockfunctions

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ import {
   graphiqlExpress
 } from 'apollo-server-express'
 import bodyParser from 'body-parser'
+import schema from './schema'
 
 const server = express()
 
-server.use('/graphiql', graphiqlExpress({
-  endpointURL: "/graphql"
-}))
+server.use('/graphiql',
+  graphiqlExpress({
+    endpointURL: "/graphql"
+  })
+)
 
-server.use('/graphql', bodyParser.json(), graphqlExpress({}))
+server.use('/graphql',
+  bodyParser.json(),
+  graphqlExpress({ schema })
+)
 
 server.listen(4000, () => {
   console.log('listening on port 4000')
