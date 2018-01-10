9f6abb8f5fb6035f441f5eef114273223b197671
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-10 14:14:40 -0500

    Add nodemon for dev

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 import express from 'express'
+
+const server = express()
+
+server.get('/graphql', (req, res) => {
+  res.send('<html><head></head><body>Hello GraphQL</body></html>')
+})
+
+server.listen(4000, () => {
+  console.log('listening on port 4000')
+})
