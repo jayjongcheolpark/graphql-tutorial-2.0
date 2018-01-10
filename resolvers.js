f2c8cb36dbee8baa179a38329fea23e47ae461db
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-10 17:30:44 -0500

    Complete delete/update

--- resolvers.js
+++ resolvers.js
@@ -4,15 +4,22 @@ import authorModel from './models/author'
 const resolvers = {
   Query: {
     authors: () => {
+      return authorModel.find({})
     },
-    author: (root, args) => {
-      return authors.find(author => author.id === args.id)
+    author: (root, { id }) => {
+      return authorModel.findOne({ id })
     }
   },
   Mutation: {
     addAuthor: (root, { name, age, books }) => {
       const author = new authorModel({ age, name, books })
       return author.save()
+    },
+    deleteAuthor: (root, { id }) => {
+      return authorModel.findOneAndRemove({ id })
+    },
+    updateAuthor: (root, { id, name }) => {
+      return authorModel.findOneAndUpdate({ id }, { name })
     }
   }
 }
