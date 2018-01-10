cc758557ff3f1d9e0bd91df561d173bf9525bc09
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-10 15:12:49 -0500

    Setup apollo-server-express

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import {
+  graphqlExpress,
+  graphiqlExpress
+} from 'apollo-server-express'
+import bodyParser from 'body-parser'
 
 const server = express()
 
-server.get('/graphql', (req, res) => {
-  res.send('<html><head></head><body>Hello GraphQL</body></html>')
-})
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: "/graphql"
+}))
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({}))
 
 server.listen(4000, () => {
   console.log('listening on port 4000')
